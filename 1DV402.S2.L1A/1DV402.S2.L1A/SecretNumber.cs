e0d949d06d3d6f0ad21a2fc2956105f0f04b1316
dn222em <dn222em@student.lnu.se>
2014-10-09 19:26:24 +0200

    klar?

---
+++ 1DV402.S2.L1A/1DV402.S2.L1A/SecretNumber.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L1A
+{
+    public class SecretNumber
+    {
+        //Fälten (Fields)
+        private int _count;
+        private int _number;
+
+        //Konstant (Constant)
+        public const int MaxNumberOfGuesses = 7;
+
+        /// <summary>
+        /// Publik metod som initierar klassens fält. 
+        /// </summary>
+        public void Initialize()
+        {
+            _count = 0;
+
+            Random numberRandom = new Random();
+            _number = numberRandom.Next(1, 101);          
+        }
+
+        /// <summary>
+        /// Publik metod som anropas för att göra en gissning av det hemliga talet. 
+        /// Beroende om det gissade talets värde, som parametern number innehåller, 
+        /// är för högt, lågt eller överensstämmer med det hemliga talet, skrivs lämpliga meddelanden ut.  
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public bool MakeGuess(int number)
+        {
+
+            
+            if (number < 1 || number > 100)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            if (_count > MaxNumberOfGuesses - 1)
+            {
+                throw new ApplicationException();
+            }
+
+            _count++;
+
+            if (number == _number)
+            {
+                Console.WriteLine("RÄTT GISSAT. Du klarade det på {0} försök.", _count); // (MaxNumberOfGuesses -(_count++)));
+                return true;
+            }
+
+            if (number < _number)
+            {
+
+                Console.WriteLine("{0} är för lågt. Du har {1} gissningar kvar.", number, MaxNumberOfGuesses - (_count)); 
+            }
+
+            if (number > _number)
+            {
+                
+                Console.WriteLine("{0} är för högt. Du har {1} gissningar kvar.",number, MaxNumberOfGuesses - (_count)); // (MaxNumberOfGuesses - (_count++)));
+            }
+          
+
+            if ( _count == MaxNumberOfGuesses)
+            {
+                Console.WriteLine("Det hemliga talet är {0}", _number);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Konstruktor (Constructor)
+        /// Konstruktorn har till uppgift att se till att ett SecretNumber-objekt är korrekt initierat. 
+        /// Det innebär att fälten har blivit tilldelade lämpliga värden, vilket enklast görs genom att låta 
+        /// konstruktorn anropa metoden Initialize(). 
+        /// </summary>
+        public SecretNumber()
+        {
+            Initialize();
+        }      
+    }
+}
